2699914435e4dc997814f7f628d7db6f242fc346
chong.song <chong.song@telink-semi.com>
2025-11-04 20:06:24 +0800

    feat(zephyr): add peripheral_kmd prj files
    feat(zephyr): merge new dualcore comm
    
    fix:(zephyr) 3228x gpio can not support GPIO_PA group
    
    feat(zephyr): peripheral_kmd modify gpios and disable no use  modules in tl3228x.overlay
    
    feat(zephyr) : Add CONFIG_BT_ID_FOR_KMD macro in id.c only used for peripheral_kmd prj
    
    feat(zephyr): Add CONFIG_BT_ID_FOR_KMD in peripheral_kmd
    
    feat: use mailbox to synchronize the mode from d25f to n22
    
    refactor(tl323x): update tl323x lib and ram arrange.
    
    - temporary optimization methods.
    
    Signed-off-by: Zhihan Tao <zhihan.tao@telink-semi.com>
    
    feat(tl322x): advance dual-core communication code position
    
    feat(tl322x): peripheral_kmd ble & 2p4g ok
    
    fix(tl322x): fix the multi-core comm wrap around error.
    
    Signed-off-by: Zhihan Tao <zhihan.tao@telink-semi.com>
    
    feat(tx3228x): peripheral_kmd usb enumeration ok
    
    feat(tl3228x) peripheral_kmd add fifo reset when mode change

--- soc/telink/tlsr/telink_tlx/soc.c
+++ soc/telink/tlsr/telink_tlx/soc.c
@@ -298,7 +298,8 @@ void soc_early_init_hook(void)
 	clock_cal_32k_rc();
 
 	/* pke is not enabled by default on TL323X */
-#if CONFIG_SOC_RISCV_TELINK_TL323X	
+#if CONFIG_SOC_RISCV_TELINK_TL323X
+	extern void pke_dig_en(void);
 	pke_dig_en();
 #endif
 
@@ -495,6 +496,7 @@ unsigned char flash_set_4line_read_write(unsigned int flash_mid)
 	return status;
 }
 #endif
+
 
 /**
  * @brief Check mounted flash size (should be greater than in .dts).
